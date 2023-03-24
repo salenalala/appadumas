1459eb031ba8f38f03210c2ca5a84d30c58e43d9
salenalala <127413697+salenalala@users.noreply.github.com>
2023-03-24 05:53:04 -0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pengaduan_masyarakat';
+$config['base_url'] = 'https://adumas.itclubigapin.my.id';
 
 /*
 |--------------------------------------------------------------------------
